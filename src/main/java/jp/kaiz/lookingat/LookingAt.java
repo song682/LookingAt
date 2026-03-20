a5424f53f876dd8ae0cc13f28e0ce4a655418fa6
dfdvdsf <81430928+song682@users.noreply.github.com>
2026-03-21 00:33:11 +0800

    🐱⬆️1.2

--- src/main/java/jp/kaiz/lookingat/LookingAt.java
+++ src/main/java/jp/kaiz/lookingat/LookingAt.java
@@ -11,14 +11,20 @@ import net.minecraft.entity.EntityList;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
+import org.lwjgl.opengl.GL11;
 
 import java.util.List;
 
-@Mod(name = LookingAt.MOD_NAME, modid = LookingAt.MOD_ID, version = LookingAt.VERSION)
+@Mod(
+        name = Tags.MOD_NAME,
+        modid = Tags.MOD_ID,
+        version = Tags.VERSION,
+        useMetadata = true,
+        acceptableRemoteVersions = Tags.MC_VERSION,
+        acceptableSaveVersions = Tags.MC_VERSION,
+        acceptedMinecraftVersions = Tags.MC_VERSION
+)
 public class LookingAt {
-    public static final String MOD_ID = "lookingat";
-    public static final String MOD_NAME = "Looking At";
-    public static final String VERSION = "1.1";
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
@@ -32,8 +38,21 @@ public class LookingAt {
         Minecraft mc = Minecraft.getMinecraft();
         if (mc.gameSettings.showDebugInfo) {
             List<String> right = event.right;
-            MovingObjectPosition mouseOver = mc.objectMouseOver;
+            List<String> left = event.left;
+            String osName = System.getProperty("os.name");
+            String osVersion = System.getProperty("os.version");
+            String osArch = System.getProperty("os.arch");
+            String CPUInfo = System.getenv("PROCESSOR_IDENTIFIER");
+            String GPUInfo = GL11.glGetString(GL11.GL_RENDERER);
+            String OpenGLVer = GL11.glGetString(GL11.GL_VERSION);
+            int cores = Runtime.getRuntime().availableProcessors();
+            right.add("Java: " + System.getProperty("java.version"));
+            right.add("CPU: " + cores + CPUInfo);
+            right.add(GPUInfo);
+            right.add("OpenGL " + OpenGLVer);
+            right.add("OS: " + osName + " " + osVersion + " (" + osArch + ")");
 
+            MovingObjectPosition mouseOver = mc.objectMouseOver;
             if (mouseOver == null) {
                 return;
             }
@@ -41,14 +60,13 @@ public class LookingAt {
             switch (mouseOver.typeOfHit) {
                 case BLOCK:
                     right.add("");
-                    right.add(ChatFormatting.UNDERLINE + String.format("Targeted Block: %s, %s, %s", mouseOver.blockX, mouseOver.blockY, mouseOver.blockZ));
+                    right.add(ChatFormatting.UNDERLINE + String.format("Targeted Block: %s %s %s", mouseOver.blockX, mouseOver.blockY, mouseOver.blockZ));
                     Block block = mc.theWorld.getBlock(mouseOver.blockX, mouseOver.blockY, mouseOver.blockZ);
                     right.add(Block.blockRegistry.getNameForObject(block));
                     break;
                 case ENTITY:
                     Entity entity = mouseOver.entityHit;
                     if (entity != null) {
-                        right.add("");
                         right.add(ChatFormatting.UNDERLINE + "Targeted Entity");
                         right.add(EntityList.getEntityString(entity));
                     }
---
+++ src/main/java/jp/kaiz/lookingat/Tags.java
@@ -0,0 +1,8 @@
+package jp.kaiz.lookingat;
+
+public class Tags {
+    public static final String MOD_ID = "lookingat";
+    public static final String MOD_NAME = "Looking At";
+    public static final String VERSION = "1.2";
+    public static final String MC_VERSION = "1.7.10";
+}
